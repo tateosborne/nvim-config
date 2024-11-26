b20b282f8b4756e51ad9520613c2e8f38bc60a06
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2024-11-26 15:07:51 -0500

    optimizing keympas

--- lua/tate/plugins/substitute.lua
+++ lua/tate/plugins/substitute.lua
@@ -11,7 +11,7 @@ return {
 
 		keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
 		keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-		keymap.set("n", "S", substitute.eol, { desc = "Substitute too end of line" })
+		keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
 		keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
 	end,
 }
