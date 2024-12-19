2fc91791db4880b0a5c831f4f7329b317ba261a9
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2024-12-18 22:58:28 -0500

    trying spaceduck theme

--- lua/tate/plugins/colorscheme.lua
+++ lua/tate/plugins/colorscheme.lua
@@ -1,11 +1,14 @@
 return {
 	--"rebelot/kanagawa.nvim",
 	--"navarasu/onedark.nvim",
-	"neanias/everforest-nvim",
+	--"neanias/everforest-nvim",
+    "pineapplegiant/spaceduck",
 	--"zenbones-theme/zenbones.nvim",
+    --dependencies = "rktjmp/lush.nvim", -- needed for zenbones
+
 	priority = 1000,
 	config = function()
 		vim.cmd("set background=dark")
-		vim.cmd("colorscheme everforest")
+		vim.cmd("colorscheme spaceduck")
 	end,
 }
--- lua/tate/plugins/lualine.lua
+++ lua/tate/plugins/lualine.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+
 	config = function()
 		local lualine = require("lualine")
 		local lazy_status = require("lazy.status")
@@ -16,6 +17,19 @@ return {
 			purple = "#d39bb6",
 			red = "#e68183",
 			grey1 = "#868d80",
+		}
+
+		local spaceduck_colors = {
+			black = "#0f111b",
+			white = "#ecf0c1",
+			red = "#e33400",
+			green = "#5ccc96",
+			blue = "#00a3cc",
+			purple = "#7a5ccc",
+			yellow = "#f2ce00",
+			gray = "#686f9a",
+			darkgray = "#30365F",
+			lightgray = "#c1c3cc",
 		}
 
 		local everforest = {
@@ -56,9 +70,48 @@ return {
 			},
 		}
 
+		local spaceduck = {
+			normal = {
+				a = { bg = spaceduck_colors.gray, fg = spaceduck_colors.black, gui = "bold" },
+				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
+				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
+			},
+			insert = {
+				a = { bg = spaceduck_colors.green, fg = spaceduck_colors.black, gui = "bold" },
+				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
+				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
+			},
+			visual = {
+				a = { bg = spaceduck_colors.yellow, fg = spaceduck_colors.black, gui = "bold" },
+				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
+				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
+			},
+			replace = {
+				a = { bg = spaceduck_colors.purple, fg = spaceduck_colors.black, gui = "bold" },
+				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
+				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
+			},
+			command = {
+				a = { bg = spaceduck_colors.blue, fg = spaceduck_colors.black, gui = "bold" },
+				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
+				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
+			},
+			terminal = {
+				a = { bg = spaceduck_colors.gray, fg = spaceduck_colors.black, gui = "bold" },
+				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
+				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
+			},
+			inactive = {
+				a = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray, gui = "bold" },
+				b = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
+				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
+			},
+		}
+
 		lualine.setup({
 			options = {
-				theme = everforest,
+				--theme = everforest,
+                theme = spaceduck,
 			},
 			sections = {
 				lualine_x = {
