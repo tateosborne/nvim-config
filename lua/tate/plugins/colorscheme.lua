583cc480b33cef274197c268778dc61b36aeda4e
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2025-02-22 23:47:31 -0500

    adjusting lualine config

--- lua/tate/plugins/colorscheme.lua
+++ lua/tate/plugins/colorscheme.lua
@@ -1,19 +1,21 @@
 return {
-	"rebelot/kanagawa.nvim",
+	-- "rebelot/kanagawa.nvim",
+	"neanias/everforest-nvim",
 	-- "sainnhe/gruvbox-material",
-	--"neanias/everforest-nvim",
-	-- "pineapplegiant/spaceduck",
-	-- "shaunsingh/nord.nvim",
+    -- "akinsho/horizon.nvim",
+    -- "nanotech/jellybeans.vim",
     -- "AlexvZyl/nordic.nvim",
-	-- "vague2k/vague.nvim",
 	-- "rose-pine/neovim",  -- rose-pine-moon for colorscheme
 
 	-- "zenbones-theme/zenbones.nvim",
-	-- dependencies = "rktjmp/lush.nvim",  -- needed for zenbones
+
+	dependencies = {
+        -- "rktjmp/lush.nvim",  -- needed for zenbones
+    },
 
 	priority = 1000,
 	config = function()
 		vim.cmd("set background=dark")
-		vim.cmd("colorscheme kanagawa")
+		vim.cmd("colorscheme everforest")
 	end,
 }
--- lua/tate/plugins/lualine.lua
+++ lua/tate/plugins/lualine.lua
@@ -6,126 +6,66 @@ return {
 		local lualine = require("lualine")
 		local lazy_status = require("lazy.status")
 
-		local ef_colors = {
-			bg0 = "#323d43",
-			bg1 = "#3c474d",
-			bg3 = "#505a60",
-			fg = "#d8caac",
-			aqua = "#87c095",
-			green = "#a7c080",
-			orange = "#e39b7b",
-			purple = "#d39bb6",
-			red = "#e68183",
-			grey1 = "#868d80",
-		}
-
-		local spaceduck_colors = {
-			black = "#0f111b",
-			white = "#ecf0c1",
-			red = "#e33400",
-			green = "#5ccc96",
-			blue = "#00a3cc",
-			purple = "#7a5ccc",
-			yellow = "#f2ce00",
-			gray = "#686f9a",
-			darkgray = "#30365F",
-			lightgray = "#c1c3cc",
-		}
-
-		local everforest = {
-			normal = {
-				a = { bg = ef_colors.green, fg = ef_colors.bg0, gui = "bold" },
-				b = { bg = ef_colors.bg3, fg = ef_colors.fg },
-				c = { bg = ef_colors.bg1, fg = ef_colors.fg },
-			},
-			insert = {
-				a = { bg = ef_colors.fg, fg = ef_colors.bg0, gui = "bold" },
-				b = { bg = ef_colors.bg3, fg = ef_colors.fg },
-				c = { bg = ef_colors.bg1, fg = ef_colors.fg },
-			},
-			visual = {
-				a = { bg = ef_colors.red, fg = ef_colors.bg0, gui = "bold" },
-				b = { bg = ef_colors.bg3, fg = ef_colors.fg },
-				c = { bg = ef_colors.bg1, fg = ef_colors.fg },
-			},
-			replace = {
-				a = { bg = ef_colors.orange, fg = ef_colors.bg0, gui = "bold" },
-				b = { bg = ef_colors.bg3, fg = ef_colors.fg },
-				c = { bg = ef_colors.bg1, fg = ef_colors.fg },
-			},
-			command = {
-				a = { bg = ef_colors.aqua, fg = ef_colors.bg0, gui = "bold" },
-				b = { bg = ef_colors.bg3, fg = ef_colors.fg },
-				c = { bg = ef_colors.bg1, fg = ef_colors.fg },
-			},
-			terminal = {
-				a = { bg = ef_colors.purple, fg = ef_colors.bg0, gui = "bold" },
-				b = { bg = ef_colors.bg3, fg = ef_colors.fg },
-				c = { bg = ef_colors.bg1, fg = ef_colors.fg },
-			},
-			inactive = {
-				a = { bg = ef_colors.bg1, fg = ef_colors.grey1, gui = "bold" },
-				b = { bg = ef_colors.bg1, fg = ef_colors.grey1 },
-				c = { bg = ef_colors.bg1, fg = ef_colors.grey1 },
-			},
-		}
-
-		local spaceduck = {
-			normal = {
-				a = { bg = spaceduck_colors.gray, fg = spaceduck_colors.black, gui = "bold" },
-				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
-				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
-			},
-			insert = {
-				a = { bg = spaceduck_colors.green, fg = spaceduck_colors.black, gui = "bold" },
-				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
-				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
-			},
-			visual = {
-				a = { bg = spaceduck_colors.yellow, fg = spaceduck_colors.black, gui = "bold" },
-				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
-				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
-			},
-			replace = {
-				a = { bg = spaceduck_colors.purple, fg = spaceduck_colors.black, gui = "bold" },
-				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
-				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
-			},
-			command = {
-				a = { bg = spaceduck_colors.blue, fg = spaceduck_colors.black, gui = "bold" },
-				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
-				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
-			},
-			terminal = {
-				a = { bg = spaceduck_colors.gray, fg = spaceduck_colors.black, gui = "bold" },
-				b = { bg = spaceduck_colors.darkgray, fg = spaceduck_colors.lightgray },
-				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
-			},
-			inactive = {
-				a = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray, gui = "bold" },
-				b = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
-				c = { bg = spaceduck_colors.black, fg = spaceduck_colors.lightgray },
-			},
-		}
-
 		lualine.setup({
 			options = {
-				--theme = everforest,
-                -- theme = spaceduck,
-                theme = "auto",
+				theme = "everforest",
+				-- theme = "gruvbox-material",
+				-- theme = "horizon",
+				-- theme = "jellybeans",
+				-- theme = "nord",
+				-- theme = "auto",
+				icons_enabled = true,
+				component_separators = { left = "󰇝", right = "󰇝" },
+				section_separators = { left = "", right = "" }, -- No section dividers
+				globalstatus = true,
 			},
 			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "diff" },
+				lualine_c = { "branch" },
 				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
-					},
-					{ "encoding" },
-					{ "fileformat" },
-					{ "filetype" },
+					function()
+						return vim.fn.system("whoami"):gsub("\n", "")
+					end,
+					"filetype",
 				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			extensions = {
+				"nvim-tree",
+				"fzf",
+				"lazy",
+				"mason",
+				"toggleterm",
+				"trouble",
+				"symbols-outline",
+				"fugitive",
+				"quickfix",
 			},
 		})
+		-- 		lualine.setup({
+		-- 			options = {
+		-- 				-- theme = "everforest",
+		-- 				-- theme = "gruvbox-material",
+		-- 				-- theme = "horizon",
+		-- 				-- theme = "jellybeans",
+		-- 				theme = "nord",
+		-- 				-- theme = "wal --theme zenburn",
+		-- 				-- theme = "auto",
+		-- 			},
+		-- 			-- 	sections = {
+		-- 			-- 		lualine_x = {
+		-- 			-- 			{
+		-- 			-- 				lazy_status.updates,
+		-- 			-- 				cond = lazy_status.has_updates,
+		-- 			-- 				color = { fg = "#ff9e64" },
+		-- 			-- 			},
+		-- 			-- 			{ "encoding" },
+		-- 			-- 			{ "fileformat" },
+		-- 			-- 			{ "filetype" },
+		-- 			-- 		},
+		-- 			-- 	},
+		-- 		})
 	end,
 }
